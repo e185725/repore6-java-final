3f301d5691b3a15fd36a12682fa423f6d9c2eeba
Toshiki Shikina <e185725@ie.u-ryukyu.ac.jp>
2019-01-14 20:39:29 +0900

    1st commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import jp.ac.uryukyu.ie.e185725.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(1);
+        Play play = new Play();
+    }
+}
---
+++ src/main/java/jp/ac/uryukyu/ie/e185725/Play.java
@@ -0,0 +1,7 @@
+package jp.ac.uryukyu.ie.e185725;
+
+public  class Play {
+    public  Play(){
+        System.out.println("aa");
+    }
+}
