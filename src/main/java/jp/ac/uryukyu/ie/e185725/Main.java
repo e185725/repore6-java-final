7327bc18f454067bb5b069cb7c83586b1d3adaf8
Toshiki Shikina <e185725@ie.u-ryukyu.ac.jp>
2019-01-18 21:57:01 +0900

    final commit

--- src/main/java/jp/ac/uryukyu/ie/e185725/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185725/Main.java
@@ -1,12 +1,23 @@
 package jp.ac.uryukyu.ie.e185725;
 
 public class Main {
+    /**
+     * ゲームを動かすクラス
+     */
     public static void main(String[] args){
+        /**
+         * int keta //桁数
+         * int [] target //シャッフルの元となるもの
+         */
 
         int keta = 3;
         int [] target;
         target = new int[]{0,1,2,3,4,5,6,7,8,9};
         EnemyNumber enemy = new EnemyNumber(keta,target);
         Play play = new Play(keta,target);
+
+        System.out.println("Numer0n!!");
+
+
     }
 }
