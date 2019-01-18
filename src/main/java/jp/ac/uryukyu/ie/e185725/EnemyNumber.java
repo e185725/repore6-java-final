a5b6c8bf6bbcecde58ca3ecc2eb83191bbfb4b3d
Toshiki Shikina <e185725@ie.u-ryukyu.ac.jp>
2019-01-18 21:32:52 +0900

    ３rd commit

---
+++ src/main/java/jp/ac/uryukyu/ie/e185725/EnemyNumber.java
@@ -0,0 +1,41 @@
+package jp.ac.uryukyu.ie.e185725;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * 答えとなる値を出力するクラス
+ */
+public class EnemyNumber {
+    /**
+     * コンストラクタ。ランダムで3桁の値を出力する
+     * @param keta　　//桁数
+     * @param targetArray    //0~9が格納されている配列
+     */
+    public  EnemyNumber(int keta, int[] targetArray) {
+        shuffle(targetArray);
+        for (int i = 0; i < keta; i++) {
+            System.out.print(targetArray[i]);//play時はコメントアウト
+        }
+        System.out.println("");
+    }
+
+    /**
+     * 配列の中の順番をランダムにするメソッド
+     * @param array  //0~9の値
+     * webサイトより引用しました↓
+     */
+    public static void shuffle(int[] array) {
+        // 配列が空か１要素ならシャッフルしようがないのので、そのままreturn
+        if (array.length <= 1) {
+            return;
+        }
+        // Fisher–Yates shuffle
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            int tmp = array[index];
+            array[index] = array[i];
+            array[i] = tmp;
+        }
+    }
+}
--- src/main/java/jp/ac/uryukyu/ie/e185725/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185725/Main.java
@@ -2,7 +2,10 @@ package jp.ac.uryukyu.ie.e185725;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println(1);
-        Play play = new Play();
+
+        int keta = 3;
+        int [] target;
+        target = new int[]{0,1,2,3,4,5,6,7,8,9};
+        EnemyNumber enemy = new EnemyNumber(keta,target);
     }
 }
